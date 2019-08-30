a0b987bda4e071d2e2d7b5782c2701f9203f1836
Feihong Hsu <feihong.hsu@gmail.com>
2019-08-29 19:20:21 -0500

    Print date and link types

--- table_view.py
+++ table_view.py
@@ -3,7 +3,7 @@ import secrets
 
 client = NotionClient(token_v2=secrets.TOKEN)
 
-url = 'https://www.notion.so/chicagochinese/19367d1bc800405fbfa5698679eec62c?v=e838eab67c3a48d1b6d47c6fe11a1e1b'
+url = 'https://www.notion.so/chicagochinese/4e06dc81541f439e841a665f9dd315bf?v=e3b7c01c8afc45389e520fd7b02872c5'
 
 page = client.get_block(url)
 
@@ -13,5 +13,6 @@ tv = client.get_collection_view(url, collection=page.collection)
 
 # Print all rows in order:
 for i, row in enumerate(tv.default_query().execute(), 1):
-  print(f'{i}. {row.title}')
-  print('   ', row.chinese_title if row.chinese_title else 'n/a')
+  print(f'{i:02d}. {row.title}')
+  print('   ', row.date.start)
+  print('   ', row.link)
